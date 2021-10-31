4e7dc989b034bcf5feaba04ad0fc18a8f4a06f23
Luke Cole <Lukeajcole@gmail.com>
2021-10-31 14:45:05 -0700

    initial commit

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,56 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_THOUGHT = gql`
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+      }
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation addComment($thoughtId: ID!, $commentText: String!) {
+    addComment(thoughtId: $thoughtId, commentText: $commentText) {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,70 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    getSingleUser: async (parent, { user = null, params }) => {
+      const foundUser = await User.findOne({
+        $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+      })
+      if (!foundUser) {
+        throw new AuthenticationError('Cannot find a user with this id!');
+      }
+      return foundUser;
+    }
+  },
+
+  Mutation: {
+    createUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      // console.log(user)
+  
+      // if (!user) {
+      //   throw new AuthenticationError('Something is wrong!');
+      // }
+      const token = signToken(user);
+      return{ token, user };
+    },
+    login: async (parent, { body }) => {
+      const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+      if (!user) {
+        throw new AuthenticationError("Can't find this user" )
+      }
+      const correctPw = await user.isCorrectPassword(body.password);
+  
+      if (!correctPw) {
+        throw new AuthenticationError('Wrong password!')
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { user, body }) => {
+      console.log(user);
+        try {
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: user._id },
+            { $addToSet: { savedBooks: body } },
+            { new: true, runValidators: true }
+          );
+          return updatedUser;
+        } catch (err) {
+          console.log(err);
+          return;
+        }
+    },
+    removeBook: async (parent, { user, params }) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: params.bookId } } },
+        { new: true }
+      );
+      if (!updatedUser) {
+        return res.status(404).json({ message: "Couldn't find user with this id!" });
+      }
+      return updatedUser;
+    },
+  },
+};
+
+module.exports = resolvers;
